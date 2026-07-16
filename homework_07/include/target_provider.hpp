2351aa2a15a43f7bb1dd5974a9f74a630fd45039
6et3ct1ve <6et3ct1ve@gmail.com>
2026-07-16 13:50:21 +0000

    feat: add JsonTargetProvider declaration

--- homework_07/include/target_provider.hpp
+++ homework_07/include/target_provider.hpp
@@ -14,3 +14,18 @@ public:
   virtual Target getTarget(int index) = 0;
   virtual ~ITargetProvider() {}
 };
+
+class JsonTargetProvider : public ITargetProvider {
+public:
+  JsonTargetProvider(const char* path, float arrayTimeStep);
+  ~JsonTargetProvider() override;
+
+  int getTargetCount() override;
+  Target getTarget(int index) override;
+
+private:
+  Coord** targets_;
+  int targetCount_;
+  int timeSteps_;
+  float arrayTimeStep_;
+};
