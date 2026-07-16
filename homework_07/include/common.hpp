d7dd60bc8dd7ad2644096be8853dd0319b926b91
6et3ct1ve <6et3ct1ve@gmail.com>
2026-07-16 12:56:35 +0000

    chore: add common.hpp with logging macros

---
+++ homework_07/include/common.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#define MAX_STEPS 10000
+#define ENABLE_LOG 1
+#define ENABLE_DEBUG 0
+
+#if ENABLE_LOG
+#define LOG(msg) std::cout << "[LOG] " << msg << std::endl
+#else
+#define LOG(msg)
+#endif
+
+#if ENABLE_DEBUG
+#define DEBUG(msg) std::cout << "[DEBUG] " << msg << std::endl
+#else
+#define DEBUG(msg)
+#endif
