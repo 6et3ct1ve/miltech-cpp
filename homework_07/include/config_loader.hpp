84cfec9a5441eaaf6aa4794d00b9113dbd85a49e
6et3ct1ve <6et3ct1ve@gmail.com>
2026-07-16 13:18:08 +0000

    refactor: apply clang-format to config_loader.hpp

--- homework_07/include/config_loader.hpp
+++ homework_07/include/config_loader.hpp
@@ -2,18 +2,20 @@
 
 #include "coord.hpp"
 
-struct AmmoParams { char name[32]; float mass, drag, lift; };
-struct DroneConfig 
-{
-	Coord startPos;
-	float altitude;
-	float initialDir;
-	float attackSpeed;
-	float accelPath;
-	char  ammoName[32];
-	float arrayTimeStep;
-	float simTimeStep;
-	float hitRadius;
-	float angularSpeed;
-	float turnThreshold;
+struct AmmoParams {
+  char name[32];
+  float mass, drag, lift;
+};
+struct DroneConfig {
+  Coord startPos;
+  float altitude;
+  float initialDir;
+  float attackSpeed;
+  float accelPath;
+  char ammoName[32];
+  float arrayTimeStep;
+  float simTimeStep;
+  float hitRadius;
+  float angularSpeed;
+  float turnThreshold;
 };
