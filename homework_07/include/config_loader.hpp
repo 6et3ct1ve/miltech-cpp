4fd0fbcd084dc1cbf18abc5afeb0845773beb034
6et3ct1ve <6et3ct1ve@gmail.com>
2026-07-16 13:14:49 +0000

    feat: add AmmoParams and DroneConfig structs

---
+++ homework_07/include/config_loader.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "coord.hpp"
+
+struct AmmoParams { char name[32]; float mass, drag, lift; };
+struct DroneConfig 
+{
+	Coord startPos;
+	float altitude;
+	float initialDir;
+	float attackSpeed;
+	float accelPath;
+	char  ammoName[32];
+	float arrayTimeStep;
+	float simTimeStep;
+	float hitRadius;
+	float angularSpeed;
+	float turnThreshold;
+};
