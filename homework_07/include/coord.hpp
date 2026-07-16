636a7d4ade628ad4cd8ccb2be42fe1c47a7a4967
6et3ct1ve <6et3ct1ve@gmail.com>
2026-07-16 13:09:42 +0000

    feat: add Coord struct with arithmetic operators

---
+++ homework_07/include/coord.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+struct Coord {
+  float x;
+  float y;
+
+  Coord operator+(const Coord& other) const
+  {
+    Coord result;
+    result.x = x + other.x;
+    result.y = y + other.y;
+    return result;
+  }
+
+  Coord operator-(const Coord& other) const
+  {
+    Coord result;
+    result.x = x - other.x;
+    result.y = y - other.y;
+    return result;
+  }
+
+  Coord operator*(float s) const
+  {
+    Coord result;
+    result.x = x * s;
+    result.y = y * s;
+    return result;
+  }
+
+  Coord operator/(float s) const
+  {
+    Coord result;
+    if (fabsf(s) < 1e-9f) {
+      std::cerr << "Div error\n";
+      return {0.0f, 0.0f};
+    }
+    else {
+      result.x = x / s;
+      result.y = y / s;
+      return result;
+    }
+  }
+
+  bool operator==(const Coord& other) const { return fabsf(x - other.x) < 1e-9f && fabsf(y - other.y) < 1e-9f; }
+};
