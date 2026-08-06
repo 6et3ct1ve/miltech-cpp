db43c4ab16fb95c206a8835aa81ca705338e1e84
6et3ct1ve <6et3ct1ve@gmail.com>
2026-08-06 09:30:10 +0000

    feat: implement DronePhysics integration and telemetry

--- homework_10/include/DronePhysics.h
+++ homework_10/include/DronePhysics.h
@@ -7,27 +7,28 @@
 
 class DronePhysics {
 public:
-    DronePhysics() = default;
-    ~DronePhysics() = default;
+  DronePhysics() = default;
+  ~DronePhysics() = default;
 
-    DronePhysics(const DronePhysics&) = delete;
-    DronePhysics& operator=(const DronePhysics&) = delete;  // NOLINT(modernize-use-trailing-return-type)
-    DronePhysics(DronePhysics&&) = delete;
-    DronePhysics& operator=(DronePhysics&&) = delete;  // NOLINT(modernize-use-trailing-return-type)
+  DronePhysics(const DronePhysics&) = delete;
+  DronePhysics& operator=(const DronePhysics&) = delete;  // NOLINT(modernize-use-trailing-return-type)
+  DronePhysics(DronePhysics&&) = delete;
+  DronePhysics& operator=(DronePhysics&&) = delete;  // NOLINT(modernize-use-trailing-return-type)
 
-    bool init(const DroneConfig& config);  // NOLINT(modernize-use-trailing-return-type)
-    void submit(const DroneCommand& command);
-    [[nodiscard]] DroneTelemetry getTelemetry() const;  // NOLINT(modernize-use-trailing-return-type)
-    void step();
+  bool init(const DroneConfig& config);  // NOLINT(modernize-use-trailing-return-type)
+  void submit(const DroneCommand& command);
+  [[nodiscard]] DroneTelemetry getTelemetry() const;  // NOLINT(modernize-use-trailing-return-type)
+  void step();
 
 private:
-    Coord pos_{};
-    float dir_ = 0.0f;
-    float speed_ = 0.0f;
-    float acceleration_ = 0.0f;
-    DroneMode mode_ = DroneMode::STOPPED;
-    float timeSecSinceStart_ = 0.0f;
-    DroneConfig config_{};
-    ThreadSafeQueue<DroneCommand> commands_;
-    mutable std::mutex mutex_;
+  Coord pos_{};
+  float dir_ = 0.0f;
+  float speed_ = 0.0f;
+  float acceleration_ = 0.0f;
+  DroneMode mode_ = DroneMode::STOPPED;
+  float timeSecSinceStart_ = 0.0f;
+  float angleSpeed_ = 0.0f;
+  DroneConfig config_{};
+  ThreadSafeQueue<DroneCommand> commands_;
+  mutable std::mutex mutex_;
 };
---
+++ homework_10/src/DronePhysics.cpp
@@ -0,0 +1,74 @@
+#include "DronePhysics.h"
+
+#include <cmath>
+#include <iostream>
+
+bool DronePhysics::init(const DroneConfig& config)
+{
+  config_ = config;
+  acceleration_ = powf(config.attackSpeed, 2.0f) / (2.0f * config.accelPath);
+  if (fabsf(acceleration_) < 1e-9f) {
+    std::cerr << "Input format error\n";
+    return false;
+  }
+  timeSecSinceStart_ = 0.0f;
+  pos_ = config.startPos;
+  dir_ = config.initialDir;
+  speed_ = config.attackSpeed;
+  mode_ = DroneMode::MOVING;
+  return true;
+}
+
+void DronePhysics::submit(const DroneCommand& command)
+{
+  commands_.push(command);
+}
+
+void DronePhysics::step()
+{
+  DroneCommand cmd{};
+  bool hasCommand = false;
+  while (commands_.tryPop(cmd)) {
+    hasCommand = true;
+  }
+
+  const std::lock_guard<std::mutex> lock(mutex_);
+
+  if (hasCommand) {
+    mode_ = cmd.mode;
+    angleSpeed_ = cmd.angleSpeed;
+  }
+
+  switch (mode_) {
+    case DroneMode::STOPPED:
+      break;
+    case DroneMode::TURNING:
+      dir_ += angleSpeed_ * config_.physicsTimeStep;
+      break;
+    case DroneMode::ACCELERATING:
+      speed_ += acceleration_ * config_.physicsTimeStep;
+      if (speed_ > config_.attackSpeed) {
+        speed_ = config_.attackSpeed;
+      }
+      break;
+    case DroneMode::DECELERATING:
+      speed_ -= acceleration_ * config_.physicsTimeStep;
+      if (speed_ < 0.0f) {
+        speed_ = 0.0f;
+      }
+      break;
+    case DroneMode::MOVING:
+      break;
+  }
+
+  pos_.x += cosf(dir_) * speed_ * config_.physicsTimeStep;
+  pos_.y += sinf(dir_) * speed_ * config_.physicsTimeStep;
+  timeSecSinceStart_ += config_.physicsTimeStep;
+}
+
+DroneTelemetry DronePhysics::getTelemetry() const
+{
+  const std::lock_guard<std::mutex> lock(mutex_);
+  return DroneTelemetry{
+    .pos = pos_, .speed = speed_, .direction = dir_, .mode = mode_, .timeSecSinceStart = timeSecSinceStart_, .acceleration = acceleration_};
+}
