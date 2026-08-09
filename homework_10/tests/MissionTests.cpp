996814968cce6d892d2bfae34ca360c1d50f8d64
6et3ct1ve <6et3ct1ve@gmail.com>
2026-08-09 11:40:20 +0000

    test: cover DronePhysics integration and clamping

--- homework_10/tests/MissionTests.cpp
+++ homework_10/tests/MissionTests.cpp
@@ -1,13 +1,16 @@
+#include "DronePhysics.h"
 #include "Types.h"
 #include "solvers/AnalyticalSolver.h"
 #include "solvers/TableSolver.h"
 #include "config/FileConfigLoader.h"
 #include "providers/ThreadSafeTargetProvider.h"
+#include "ThreadSafeQueue.h"
+#include <thread>
 #include <gtest/gtest.h>
 
 // NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 
-TEST(Hw08Coord, Addition)
+TEST(Hw10Coord, Addition)
 {
   Coord a{1.0f, 2.0f};
   Coord b{3.0f, 4.0f};
@@ -16,7 +19,7 @@ TEST(Hw08Coord, Addition)
   EXPECT_NEAR(result.y, 6.0f, 0.001f);
 }
 
-TEST(Hw08Coord, Subtraction)
+TEST(Hw10Coord, Subtraction)
 {
   Coord a{5.0f, 5.0f};
   Coord b{2.0f, 1.0f};
@@ -25,7 +28,7 @@ TEST(Hw08Coord, Subtraction)
   EXPECT_NEAR(result.y, 4.0f, 0.001f);
 }
 
-TEST(Hw08Coord, ScalarMultiplication)
+TEST(Hw10Coord, ScalarMultiplication)
 {
   Coord a{2.0f, 3.0f};
   Coord result = a * 2.0f;
@@ -33,7 +36,7 @@ TEST(Hw08Coord, ScalarMultiplication)
   EXPECT_NEAR(result.y, 6.0f, 0.001f);
 }
 
-TEST(Hw08Coord, ScalarDivision)
+TEST(Hw10Coord, ScalarDivision)
 {
   Coord a{6.0f, 8.0f};
   Coord result = a / 2.0f;
@@ -41,7 +44,7 @@ TEST(Hw08Coord, ScalarDivision)
   EXPECT_NEAR(result.y, 4.0f, 0.001f);
 }
 
-TEST(Hw08Coord, DivisionByZeroReturnsZero)
+TEST(Hw10Coord, DivisionByZeroReturnsZero)
 {
   Coord a{6.0f, 8.0f};
   Coord result = a / 0.0f;
@@ -49,14 +52,14 @@ TEST(Hw08Coord, DivisionByZeroReturnsZero)
   EXPECT_NEAR(result.y, 0.0f, 0.001f);
 }
 
-TEST(Hw08Coord, Equality)
+TEST(Hw10Coord, Equality)
 {
   Coord a{1.0f, 2.0f};
   Coord b{1.0f, 2.0f};
   EXPECT_TRUE(a == b);
 }
 
-TEST(Hw08AnalyticalSolver, ComputesKnownDropPoint)
+TEST(Hw10AnalyticalSolver, ComputesKnownDropPoint)
 {
   AnalyticalSolver solver;
   Coord dronePos{0.0f, 0.0f};
@@ -76,7 +79,7 @@ TEST(Hw08AnalyticalSolver, ComputesKnownDropPoint)
   EXPECT_NEAR(h, 66.576f, 0.1f);
 }
 
-TEST(Hw08AnalyticalSolver, SamePositionFails)
+TEST(Hw10AnalyticalSolver, SamePositionFails)
 {
   AnalyticalSolver solver;
   Coord dronePos{0.0f, 0.0f};
@@ -90,33 +93,33 @@ TEST(Hw08AnalyticalSolver, SamePositionFails)
   EXPECT_FALSE(ok);
 }
 
-TEST(Hw08FileConfigLoader, MissingFileFails)
+TEST(Hw10FileConfigLoader, MissingFileFails)
 {
   FileConfigLoader loader("nonexistent_config.json", "nonexistent_ammo.json");
   EXPECT_FALSE(loader.load());
 }
 
-TEST(Hw08JsonTargetProvider, MissingFileIsInvalid)
+TEST(Hw10ThreadSafeTargetProvider, MissingFileIsInvalid)
 {
-  JsonTargetProvider provider("nonexistent_targets.json", 1.0f);
+  ThreadSafeTargetProvider provider("nonexistent_targets.json", 1.0f, 1.0f);
   EXPECT_FALSE(provider.isValid());
 }
 
-TEST(Hw09TableSolver, MissingFileIsInvalid)
+TEST(Hw10TableSolver, MissingFileIsInvalid)
 {
   TableSolver solver("nonexistent_table.txt");
   EXPECT_FALSE(solver.isValid());
 }
 
-TEST(Hw09TableSolver, LoadsRealTable)
+TEST(Hw10TableSolver, LoadsRealTable)
 {
-  TableSolver solver("../../../homework_09/data/ballistic_table.txt");
+  TableSolver solver("../../../homework_10/data/ballistic_table.txt");
   EXPECT_TRUE(solver.isValid());
 }
 
-TEST(Hw09TableSolver, ComputesDropPointAtExactNode)
+TEST(Hw10TableSolver, ComputesDropPointAtExactNode)
 {
-  TableSolver solver("../../../homework_09/data/ballistic_table.txt");
+  TableSolver solver("../../../homework_10/data/ballistic_table.txt");
   ASSERT_TRUE(solver.isValid());
 
   Coord dronePos{0.0f, 0.0f};
@@ -132,4 +135,87 @@ TEST(Hw09TableSolver, ComputesDropPointAtExactNode)
   EXPECT_NEAR(result.x, 297.106f, 0.01f);
   EXPECT_NEAR(result.y, 0.0f, 0.01f);
 }
+
+TEST(Hw10ThreadSafeQueue, PopOnEmptyReturnsFalse)
+{
+  ThreadSafeQueue<DroneCommand> queue;
+  DroneCommand out{};
+  EXPECT_FALSE(queue.tryPop(out));
+}
+
+TEST(Hw10ThreadSafeQueue, PushThenPopReturnsSameValue)
+{
+  ThreadSafeQueue<DroneCommand> queue;
+  DroneCommand cmd{DroneMode::TURNING, 0.5f};
+  queue.push(cmd);
+
+  DroneCommand out{};
+  EXPECT_TRUE(queue.tryPop(out));
+  EXPECT_EQ(out.mode, DroneMode::TURNING);
+  EXPECT_NEAR(out.angleSpeed, 0.5f, 0.001f);
+}
+
+TEST(Hw10ThreadSafeQueue, PreservesFifoOrder)
+{
+  ThreadSafeQueue<DroneCommand> queue;
+  DroneCommand cmd1{DroneMode::TURNING, 0.5f};
+  DroneCommand cmd2{DroneMode::MOVING, 1.0f};
+  DroneCommand cmd3{DroneMode::ACCELERATING, 0.0f};
+  queue.push(cmd1);
+  queue.push(cmd2);
+  queue.push(cmd3);
+
+  DroneCommand out1{};
+  DroneCommand out2{};
+  DroneCommand out3{};
+  EXPECT_TRUE(queue.tryPop(out1));
+  EXPECT_TRUE(queue.tryPop(out2));
+  EXPECT_TRUE(queue.tryPop(out3));
+  EXPECT_EQ(out1.mode, DroneMode::TURNING);
+  EXPECT_NEAR(out1.angleSpeed, 0.5f, 0.001f);
+  EXPECT_EQ(out2.mode, DroneMode::MOVING);
+  EXPECT_NEAR(out2.angleSpeed, 1.0f, 0.001f);
+  EXPECT_EQ(out3.mode, DroneMode::ACCELERATING);
+  EXPECT_NEAR(out3.angleSpeed, 0.0f, 0.001f);
+}
+
+TEST(Hw10ThreadSafeQueue, SurvivesConcurrentAccess)
+{
+  constexpr int kCount = 1000;
+  ThreadSafeQueue<DroneCommand> queue;
+
+  std::thread producer([&queue]() {
+    for (int i = 0; i < kCount; i++) {
+      queue.push(DroneCommand{DroneMode::MOVING, static_cast<float>(i)});
+    }
+  });
+
+  int received = 0;
+  DroneCommand out{};
+  while (received < kCount) {
+    if (queue.tryPop(out)) {
+      received++;
+    }
+  }
+
+  producer.join();
+  EXPECT_EQ(received, kCount);
+}
+
+TEST(Hw10DronePhysics, InitFailsOnZeroAccelPath)
+{
+  DroneConfig config{};
+  config.attackSpeed = 15.0f;
+  config.accelPath = 0.0f;
+  DronePhysics physics;
+  EXPECT_FALSE(physics.init(config));
+}
+
+TEST(Hw10DronePhysics, MovingIntegratesPosition) {}
+
+TEST(Hw10DronePhysics, TurningChangesDirection) {}
+
+TEST(Hw10DronePhysics, AcceleratingClampsAtAttackSpeed) {}
+
+TEST(Hw10DronePhysics, DeceleratingClampsAtZero) {}
 // NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
